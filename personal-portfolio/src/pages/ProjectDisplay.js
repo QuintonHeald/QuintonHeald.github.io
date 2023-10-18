44d1727551aa447037b9ac2fbb5a8fd6d91c5eb6
Quinton Heald <qh185011@ncr.com>
2023-10-18 03:41:32 -0400

    Fixed issues regarding individual project pages

--- personal-portfolio/src/pages/ProjectDisplay.js
+++ personal-portfolio/src/pages/ProjectDisplay.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { ProjectList } from '../helpers/ProjectLists';
-import { GitHub } from '@material-ui/icons';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ProjectList } from "../helpers/ProjectList";
+import GitHub from "@material-ui/icons/GitHub";
 import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
-    const { id } = useParams();
-    const project = ProjectList[id];
-    return (
-        <div className='project'>
-            <h1> {project.name}</h1>
-            <img src={project.img} alt='ProjectName' />
-            <p>
-                <b>Skills:</b> {project.skills} 
-            </p>
-            <GitHub />
-        </div>
-    );
+  const { id } = useParams();
+  const project = ProjectList[id];
+  return (
+    <div className="project">
+      <h1> {project.name}</h1>
+      <img src={project.image} alt="Project"/>
+      <p>
+        <b>Skills:</b> {project.skills}
+      </p>
+      <GitHub />
+    </div>
+  );
 }
 
-export default ProjectDisplay
+export default ProjectDisplay;
